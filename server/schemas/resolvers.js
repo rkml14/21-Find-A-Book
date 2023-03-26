ef727b29375f35efece2e93190c506339a852540
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-03-26 13:43:57 -0400

    updated devtyps

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,12 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+const { User, Book } = require('../models');
+
+const resolvers = {
+    Query: {
+
+    }
+};
+
+
+module.exports = resolvers;
